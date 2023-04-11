12e9498334b633b13b12a2cd046e7d04013dfc23
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-11 14:03:25 +0500

    Implement StringExtension to hash user password with creating Extentions folder

---
+++ FleetFlow.Service/Extentions/StringExtention.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetFlow.Service.Extentions
+{
+    public static class StringExtention
+    {
+        public static string Encrypt(this string password)
+        {
+            using(SHA256 sha256HASH = SHA256.Create())
+            {
+                var hashedBytes = sha256HASH.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                var hashedPassword = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+
+                return hashedPassword;
+            }
+        }
+    }
+}
