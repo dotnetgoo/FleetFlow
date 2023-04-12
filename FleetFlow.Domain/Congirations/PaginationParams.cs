01c56791bb1a06c916d1850bbeef5724e389d8f8
Muxammad <muxammadcodes@gmail.com>
2023-04-12 22:30:24 +0500

    Paginations Added

--- FleetFlow.Domain/Congirations/PaginationParams.cs
+++ FleetFlow.Domain/Congirations/PaginationParams.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection.Metadata;
 
-namespace FleetFlow.Domain.Congirations
+namespace FleetFlow.Domain.Congirations;
+
+public class PaginationParams
 {
-    internal class PaginationParams
+    private const int _maxPageSize = 10;
+    private int _pageSize;
+    public int PageSize
     {
+        get => _pageSize;
+        set => _pageSize = value > _maxPageSize ? _maxPageSize : value;
     }
+    public int PageIndex { get; set; }
 }
