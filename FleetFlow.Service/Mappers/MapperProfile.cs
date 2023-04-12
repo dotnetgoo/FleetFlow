8e84e42b4dc9dc10c32995d3f750b0b5abdc55e5
Muxammad <muxammadcodes@gmail.com>
2023-04-12 22:09:58 +0500

    Product Service added with DTOs and AutoMapper

---
+++ FleetFlow.Service/Mappers/MapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FleetFlow.Domain.Entities;
+using FleetFlow.Service.DTOs;
+
+namespace FleetFlow.Service.Mappers
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Product,ProductCreationDto>().ReverseMap();
+        }
+    }
+}
