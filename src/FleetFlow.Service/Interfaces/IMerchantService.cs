c5ee2c496af509f2d492b7d48c9819d4463202c3
komron2052@gmail.com <komron2052@gmail.com>
2023-04-16 11:47:31 +0500

    Implemented merchant service

---
+++ src/FleetFlow.Service/Interfaces/IMerchantService.cs
@@ -0,0 +1,13 @@
+﻿using FleetFlow.Domain.Congirations;
+using FleetFlow.Service.DTOs;
+
+namespace FleetFlow.Service.Interfaces;
+
+public interface IMerchantService
+{
+    Task<MerchantForResultDto> AddAsync(MerchantForCreationDto dto);
+    Task<MerchantForResultDto> ModifyAsync(long id, MerchantForCreationDto dto);
+    Task<bool> RemoveAsync(long id);
+    Task<MerchantForResultDto> RetrieveByIdAsync(long id);
+    Task<IEnumerable<MerchantForResultDto>> RetrieveAllAsync(PaginationParams @params);
+}
