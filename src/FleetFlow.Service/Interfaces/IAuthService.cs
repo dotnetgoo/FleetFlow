8e1efb822c5fca1537508da677fd11f2b2d2d934
Axmadjonov Abdulloh <abdulloh.7013@gmail.com>
2023-04-27 15:39:07 +0500

    Added AuthService

---
+++ src/FleetFlow.Service/Interfaces/IAuthService.cs
@@ -0,0 +1,6 @@
+﻿namespace FleetFlow.Service.Interfaces;
+
+public interface IAuthService
+{
+    Task<string> GenerateTokenAsync(string email, string password);
+}
---
+++ src/FleetFlow.Service/Services/AuthService.cs
@@ -0,0 +1,43 @@
+﻿using FleetFlow.Service.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace FleetFlow.Service.Services;
+
+public class AuthService : IAuthService
+{
+    private IUserService userService;
+    private IConfiguration configuration;
+
+    public AuthService(IUserService userService, IConfiguration configuration)
+    {
+        this.userService = userService;
+        this.configuration = configuration;
+    }
+
+    public async Task<string> GenerateTokenAsync(string email, string password)
+    {
+        var user = await userService.CheckUserAsync(email, password);
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenKey = Encoding.UTF8.GetBytes(configuration["JWT:Key"]);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+             new Claim("Id", user.Id.ToString()),
+             new Claim(ClaimTypes.Role, user.Role.ToString()),
+             new Claim(ClaimTypes.Name, user.FirstName)
+            }),
+            IssuedAt = DateTime.UtcNow,
+            Expires = DateTime.UtcNow.AddSeconds(20),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
