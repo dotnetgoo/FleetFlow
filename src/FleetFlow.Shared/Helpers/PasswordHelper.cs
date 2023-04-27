514dea9ac551df23868c4c3f1ca8b7413eb71634
MuzaffarNurillaew <nurillaewmuzaffar@gmail.com>
2023-04-27 17:07:32 +0500

    AuthService, Shared layer

---
+++ src/FleetFlow.Shared/Helpers/PasswordHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetFlow.Shared.Helpers
+{
+    public class PasswordHelper
+    {
+        /// <summary>
+        /// Hash password 
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        /// <summary>
+        /// Verify its correct
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="passwordHash"></param>
+        /// <returns></returns>
+        public static bool Verify(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
