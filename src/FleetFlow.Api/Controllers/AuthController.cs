6184c3a5d4e8f2d6e516ad7e44365da84decc7f5
AbdulloMic <84926707+kerakmas@users.noreply.github.com>
2023-04-27 16:01:57 +0500

    AuthController added

---
+++ src/FleetFlow.Api/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using FleetFlow.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FleetFlow.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate(string username, string password)
+        {
+            return Ok(await this._authService.GenerateTokenAsync(username, password));
+            
+        }
+    }
+}
