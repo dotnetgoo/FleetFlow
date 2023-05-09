9ffea465a547f14c4de22634b39678bb09039f5b
Muxammad <muxammadcodes@gmail.com>
2023-05-09 09:48:31 +0500

    Paginetion Meta Data

--- src/FleetFlow.Service/Extensions/CollectionExtensions.cs
+++ src/FleetFlow.Service/Extensions/CollectionExtensions.cs
@@ -9,6 +9,8 @@ namespace FleetFlow.Service.Extentions
         public static IQueryable<TEntity> ToPagedList<TEntity>(this IQueryable<TEntity> entities, PaginationParams @params)
             where TEntity : Auditable
         {
+
+        
             return @params.PageIndex > 0 && @params.PageSize > 0 ?
                 entities.OrderBy(e => e.Id)
                     .Skip((@params.PageIndex - 1) * @params.PageSize).Take(@params.PageSize) :
