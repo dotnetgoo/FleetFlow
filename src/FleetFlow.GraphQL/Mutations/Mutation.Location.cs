5438da08d8bcb0952f52a6ff28664fb52a2c5b93
Abdulloh <muhamedovabdulloh26@gmail.com>
2023-05-11 20:24:21 +0500

    ADDED: Mutation.Address and Mutation.Location

---
+++ src/FleetFlow.GraphQL/Mutations/Mutation.Location.cs
@@ -0,0 +1,28 @@
+﻿using FleetFlow.Service.DTOs.Location;
+using FleetFlow.Service.Interfaces;
+using HotChocolate;
+
+namespace FleetFlow.GraphQL.Mutations
+{
+    public partial class Mutation
+    {
+        public async ValueTask<LocationForResultDto> CreateLocationAsync([Service] ILocationService locationService,
+            LocationForCreationDto location)
+        {
+            return await locationService.AddAsync(location);
+        }
+
+        public async ValueTask<bool> DeleteLocationAsync([Service] ILocationService locationService,
+            long id)
+        {
+            return await locationService.RemoveAsync(id);
+        }
+
+        public async ValueTask<LocationForResultDto> UpdateLocationAsync([Service] ILocationService locationService,
+            long id,
+            LocationForCreationDto location)
+        {
+            return await locationService.ModifyAsync(id, location);
+        }
+    }
+}
