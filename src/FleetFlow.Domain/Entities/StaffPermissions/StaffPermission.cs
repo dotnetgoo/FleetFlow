3d0e9fcab09bf3b1a518c6b2280b9db26d41fe57
Javlonbek fayzullaev <fayzullaev.javlonbek.d@gmail.com>
2023-05-25 22:42:13 +0500

    Added Staff and StaffPermission entities

---
+++ src/FleetFlow.Domain/Entities/StaffPermissions/StaffPermission.cs
@@ -0,0 +1,20 @@
+﻿using FleetFlow.Domain.Commons;
+using FleetFlow.Domain.Entities.Authorizations;
+using FleetFlow.Domain.Entities.Staffs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetFlow.Domain.Entities.StaffPermissions
+{
+    public class StaffPermission : Auditable
+    {
+        public int StaffId { get; set; }
+        public Staff Staff { get; set; }
+        public int PermissionId { get; set; }
+        public Permission Permission { get; set; }
+
+    }
+}
---
+++ src/FleetFlow.Domain/Entities/Staffs/Staff.cs
@@ -0,0 +1,19 @@
+﻿using FleetFlow.Domain.Commons;
+using FleetFlow.Domain.Entities.Authorizations;
+using FleetFlow.Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetFlow.Domain.Entities.Staffs
+{
+    public class Staff : Auditable
+    {
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
