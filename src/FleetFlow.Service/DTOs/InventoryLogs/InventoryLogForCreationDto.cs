87fac323fbc884bc4cf69a5bb98007fb335a6546
Javlonbek fayzullaev <fayzullaev.javlonbek.d@gmail.com>
2023-05-25 16:46:40 +0500

    Added intentory and logs dtos

---
+++ src/FleetFlow.Service/DTOs/InventoryLogs/InventoryLogForCreationDto.cs
@@ -0,0 +1,10 @@
+﻿using FleetFlow.Domain.Commons;
+
+namespace FleetFlow.Service.DTOs.InventoryLogs
+{
+    public class InventoryLogForCreationDto : Auditable
+    {
+        public long ProductId { get; set; }
+        public int Amount { get; set; }
+    }
+}
