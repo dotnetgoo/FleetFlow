c654ff4a16eb4f1563e370d91d4c46adb13c7563
Asadbek Zaylobiddinov <asadbekzaylobiddinov992@gmail.com>
2023-05-25 20:21:42 +0500

    Adding Queries for Permission, Role and RolePermission

---
+++ src/FleetFlow.GraphQL/Queries/Query.Permission.cs
@@ -0,0 +1,21 @@
+﻿using FleetFlow.Domain.Congirations;
+using FleetFlow.Service.DTOs.Discounts;
+using FleetFlow.Service.DTOs.Permissions;
+using FleetFlow.Service.Interfaces.Authorizations;
+
+namespace FleetFlow.GraphQL.Queries
+{
+    public partial class Query
+    {
+        public async ValueTask<PermissionForResultDto> GetPermissionByIdAsync([Service] IPermissionService permissionService, long id)
+        {
+            return await permissionService.RetrieveByIdAsync(id);
+        }
+
+        public async ValueTask<IEnumerable<PermissionForResultDto>> GetAllPermissionsAsync([Service] IPermissionService permissionService,
+            PaginationParams @params)
+        {
+            return await permissionService.RetrieveAllAsync(@params);
+        }
+    }
+}
