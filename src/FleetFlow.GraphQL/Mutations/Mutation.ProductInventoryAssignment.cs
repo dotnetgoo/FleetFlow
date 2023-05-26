b704c2de28462e0be179e309a8cd17457f6834a0
Bekmurod <113014649+Bekmurod21@users.noreply.github.com>
2023-05-26 23:23:58 +0500

    ProductInventoryAssignment Graphql completed

---
+++ src/FleetFlow.GraphQL/Mutations/Mutation.ProductInventoryAssignment.cs
@@ -0,0 +1,35 @@
+﻿using FleetFlow.Service.DTOs.Inventories;
+using FleetFlow.Service.Interfaces.Warehouses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FleetFlow.GraphQL.Mutations
+{
+    public partial class Mutation
+    {
+        public async ValueTask<ProductInventoryAssignmentForResultDto> CreateProductInventoryAssignmentAsync([Service]
+        IProductInventoryAssignmentService service, ProductInventoryAssignmentForCreationDto dto)
+        {
+            return await service.AddAsync(dto);
+        }
+        public async ValueTask<ProductInventoryAssignmentForResultDto> UpdateProductInventoryAssignment([Service]
+        IProductInventoryAssignmentService service,long id,ProductInventoryAssignmentForUpdateDto dto)
+        {
+            return await service.ModifyAsync(id,dto);
+        }
+        public async ValueTask<bool> DeleteProductInventoryAssignment([Service] IProductInventoryAssignmentService service,
+            long id)
+        {
+            return await service.RemoveAsync(id);
+        }
+        public async ValueTask<ProductInventoryAssignmentForResultDto> CreateQuantity([Service] IProductInventoryAssignmentService service,
+            long productId, long inventoryId, int amount)
+        {
+            return await service.AddQuantity(productId, inventoryId, amount);
+        }
+        public async ValueTask<ProductInventoryAssignmentForResultDto> DeleteQuantity([Service] IProductInventoryAssignmentService service,
+           long productId, long inventoryId, int amount)
+        {
+            return await service.RemoveQuantity(productId, inventoryId, amount);
+        }
+    }
+}
---
+++ src/FleetFlow.GraphQL/Queries/Query.ProductInventoryAssignment.cs
@@ -0,0 +1,25 @@
+﻿using FleetFlow.Domain.Congirations;
+using FleetFlow.Service.DTOs.Inventories;
+using FleetFlow.Service.Interfaces.Warehouses;
+
+namespace FleetFlow.GraphQL.Queries
+{
+    public partial class Query
+    {
+        public async ValueTask<ProductInventoryAssignmentForResultDto> GetProductInventoryAssignmentById([Service]
+        IProductInventoryAssignmentService service,long id)
+        {
+            return await service.RetrieveByIdAsync(id);
+        }
+        public async ValueTask<IEnumerable<ProductInventoryAssignmentForResultDto>> GetAllProductInventoryAssignment(
+            [Service]IProductInventoryAssignmentService service,PaginationParams @params)
+        {
+            return await service.RetrieveAllAsync(@params);
+        }
+        public async ValueTask<IEnumerable<ProductInventoryAssignmentForResultDto>> GetProductByIdAsync([Service]
+        IProductInventoryAssignmentService service,long id)
+        {
+            return await service.RetrieveProductById(id);
+        }
+    }
+}
