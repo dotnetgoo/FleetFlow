5f59956839fd610b630223265460e78420e6316b
Behzodkhoja <nigmatxojayevbehzodxoja@gmail.com>
2023-05-26 17:09:06 +0500

    Created Middleware and Responce for GraphQL

---
+++ src/FleetFlow.GraphQL/Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace FleetFlow.GraphQL.Models
+{
+    public class Response
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
