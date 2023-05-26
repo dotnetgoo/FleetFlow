b8b34643ec8666378746069efae8569e18f432a4
AlJavhar <My274737@gmail.com>
2023-05-26 18:31:33 +0500

    Add Order finish

--- src/FleetFlow.Service/Services/Warehouses/ProductInventoryAssignmentService.cs
+++ src/FleetFlow.Service/Services/Warehouses/ProductInventoryAssignmentService.cs
@@ -74,7 +74,7 @@ namespace FleetFlow.Service.Services.Warehouses
         }
         public async Task<ProductInventoryAssignmentForResultDto> RemoveQuantity(long ProductId, long InventoryId, int amount)
         {
-            var model = await this.repository.SelectAsync(x => x.ProductId == ProductId && x.
+            var model = await this.repository.SelectAsync(x => x.ProductId == ProductId && x.InventoryId
             == InventoryId);
             if (model is null || model.IsDeleted == true)
                 throw new FleetFlowException(404, "Product not found");
