9f0cbcaa58e512c6952b2137d7173f15e86cd904
Abdulloh <muhamedovabdulloh26@gmail.com>
2023-05-26 17:54:27 +0500

    Updates

--- src/FleetFlow.Api/Controllers/AnswerController.cs
+++ src/FleetFlow.Api/Controllers/AnswerController.cs
@@ -14,6 +14,11 @@ namespace FleetFlow.Api.Controllers
         {
             this.answerService = answerService;
         }
+        /// <summary>
+        /// Add Asnwer for the given question
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
         [HttpPost("answer")]
         public async ValueTask<IActionResult> AddAsnwerAsync([FromBody] AnswerForCreationDto dto)
             => Ok(new Response
@@ -23,6 +28,11 @@ namespace FleetFlow.Api.Controllers
                 Data = await this.answerService.AddAsync(dto)
             });
 
+        /// <summary>
+        /// Delete Answer by id from database
+        /// </summary>
+        /// <param name="answerId"></param>
+        /// <returns></returns>
         [HttpDelete("{id:long}")]
         public async ValueTask<IActionResult> DeleteAnswerAsync([FromRoute(Name = "id")] long answerId)
             => Ok(new Response
@@ -32,6 +42,11 @@ namespace FleetFlow.Api.Controllers
                 Data = await this.answerService.DeleteByIdAsync(answerId)
             });
 
+        /// <summary>
+        /// Get Answer by id
+        /// </summary>
+        /// <param name="answerId"></param>
+        /// <returns></returns>
         [HttpGet("{id:long}")]
         public async ValueTask<IActionResult> GetByIdAsync([FromRoute(Name = "id")] long answerId)
             => Ok(new Response
@@ -41,6 +56,11 @@ namespace FleetFlow.Api.Controllers
                 Data = await this.answerService.GetByIdAsync(answerId)
             });
 
+        /// <summary>
+        /// Get All Answers
+        /// </summary>
+        /// <param name="params"></param>
+        /// <returns></returns>
         [HttpGet("answers")]
         public async ValueTask<IActionResult> GetAllAsync([FromQuery] PaginationParams @params)
             => Ok(new Response
@@ -50,6 +70,12 @@ namespace FleetFlow.Api.Controllers
                 Data = await this.answerService.GetAllAsync(@params)
             });
 
+        /// <summary>
+        /// Get All by AdminId
+        /// </summary>
+        /// <param name="params"></param>
+        /// <param name="adminId"></param>
+        /// <returns></returns>
         [HttpGet("admin-id")]
         public async ValueTask<IActionResult> GetAllByAdminIdAsync([FromQuery] PaginationParams @params, long adminId)
             => Ok(new Response
@@ -59,6 +85,12 @@ namespace FleetFlow.Api.Controllers
                 Data = await this.answerService.GetAllByAdminIdAsync(@params, adminId)
             });
 
+        /// <summary>
+        /// Get All by UserId
+        /// </summary>
+        /// <param name="params"></param>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         [HttpGet("user-id")]
         public async ValueTask<IActionResult> GetAllByUserIdAsync([FromQuery] PaginationParams @params, long userId)
             => Ok(new Response
@@ -68,6 +100,12 @@ namespace FleetFlow.Api.Controllers
                 Data = await this.answerService.GetAllByUserIdAsync(@params, userId)
             });
 
+        /// <summary>
+        /// Edit Answer by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="dto"></param>
+        /// <returns></returns>
         [HttpPut("{id:long}")]
         public async ValueTask<IActionResult> UpdateAnswerAsync([FromRoute]long id, AnswerForCreationDto dto)
             => Ok(new Response
