20251b8d28ee3c10ab9b46bbfda5423626485184
Nozimjon Usmonaliyev <usminaliyevnozimjon@gmail.com>
2023-05-27 17:51:43 +0500

    issues on rolepermission and permission services have been resolved

--- src/FleetFlow.Service/Interfaces/Authorizations/IRolePermissionService.cs
+++ src/FleetFlow.Service/Interfaces/Authorizations/IRolePermissionService.cs
@@ -9,9 +9,8 @@ public interface IRolePermissionService
 {
 	Task<RolePermissionForResultDto> CreateAsync(RolePermissionForCreateDto permission);
 	Task<RolePermissionForResultDto> ModifyAsync(RolePermissionForUpdateDto permission);
-	Task<bool> DeleteAsync(long id);
+	Task<bool> RemoveAsync(long id);
 	Task<RolePermissionForResultDto> RetrieveByIdAsync(long id);
 	Task<List<RolePermissionForResultDto>> RetrieveAllAsync(PaginationParams @params);
-	Task<List<RolePermissionForResultDto>> RetrieveAllPermissionsByRole(long roleId, string role);
 	Task<bool> CheckPermission(string role, string accessedMethod); 
 }
