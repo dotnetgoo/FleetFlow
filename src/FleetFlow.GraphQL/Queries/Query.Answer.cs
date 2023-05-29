7d981ed50778624e5f9ebce90d0e1aaa7c30f44e
Abdulloh <muhamedovabdulloh26@gmail.com>
2023-05-29 09:44:50 +0500

    Queries added for Answer and Insight

---
+++ src/FleetFlow.GraphQL/Queries/Query.Answer.cs
@@ -0,0 +1,35 @@
+﻿using FleetFlow.Domain.Congirations;
+using FleetFlow.Domain.Entities.UserQuestions;
+using FleetFlow.Service.Interfaces.UserQuestions;
+
+namespace FleetFlow.GraphQL.Queries
+{
+    public partial class Query
+    {
+        public async ValueTask<Answer> GetByIdAsync([Service] IAnswerService answerService,
+            long id)
+        {
+            return await answerService.RetrieveByIdAsync(id);
+        }
+
+        public async ValueTask<IEnumerable<Answer>> GetAllAsync([Service] IAnswerService answerService,
+            PaginationParams @params)
+        {
+            return await answerService.RetrieveAllAsync(@params);
+        }
+
+        public async ValueTask<IEnumerable<Answer>> GetAllByUserIdAsync([Service] IAnswerService answerService,
+            PaginationParams @params,
+            long userId)
+        {
+            return await answerService.RetrieveAllByUserIdAsync(@params, userId);
+        }
+
+        public async ValueTask<IEnumerable<Answer>> GetAllByAdminIdAsync([Service] IAnswerService answerService,
+            PaginationParams @params,
+            long adminId)
+        {
+            return await answerService.RetrieveAllByAdminIdAsync(@params, adminId);
+        }
+    }
+}
