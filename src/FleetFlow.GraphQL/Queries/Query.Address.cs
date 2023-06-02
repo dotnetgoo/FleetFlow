4b96dca355f691d6ffd41b8794e6d11522566fa9
Bekmurod Boqiyev <bekmurodboqiyev21@gmail.com>
2023-06-02 17:43:18 +0500

    Address,Checkout,District,Inventory,Location,Order  Graphql queries completed

---
+++ src/FleetFlow.GraphQL/Queries/Query.Address.cs
@@ -0,0 +1,19 @@
+﻿using FleetFlow.Domain.Congirations;
+using FleetFlow.Domain.Entities.Addresses;
+using FleetFlow.Service.DTOs.Address;
+using FleetFlow.Service.Interfaces.Addresses;
+
+namespace FleetFlow.GraphQL.Queries
+{
+    public partial class Query
+    {
+        public async ValueTask<AddressForResultDto> GetAddressByIdAsync([Service] IAddressService service,long id)
+        {
+            return await service.GetByIdAsync(id);
+        }
+        public async ValueTask<IEnumerable<AddressForResultDto>> GetAdressAll([Service] IAddressService service,PaginationParams @params)
+        {
+            return await service.GetAllAsync (@params);
+        }
+    }
+}
---
+++ src/FleetFlow.GraphQL/Queries/Query.Order.cs
@@ -0,0 +1,28 @@
+﻿using FleetFlow.Domain.Enums;
+using FleetFlow.Service.DTOs.Orders;
+using FleetFlow.Domain.Congirations;
+using FleetFlow.Service.Interfaces.Orders;
+
+namespace FleetFlow.GraphQL.Queries
+{
+    public partial class Query
+    {
+        public async ValueTask<OrderResultDto> GetOrderByIdAsync([Service] IOrderService service,long id)
+        {
+            return await service.RetrieveAsync(id);
+        }
+        public async ValueTask<IEnumerable<OrderResultDto>> GetOrderAllAsync([Service] IOrderService service,PaginationParams @params)
+        {
+            return await service.RetrieveAllAsync(@params);
+        }
+        public async ValueTask<IEnumerable<OrderResultDto>> GetOrderAllByClientIdAsync([Service] IOrderService service, long clientId)
+        {
+            return await service.RetrieveAllByClientIdAsync(clientId);
+        }
+        public async ValueTask<IEnumerable<OrderResultDto>> GetOrderAllByPhoneAsync([Service] IOrderService service, PaginationParams @params,
+            string phone, OrderStatus? status = null)
+        {
+            return await service.RetrieveAllByPhoneAsync(@params,phone, status);
+        }
+    }
+}
