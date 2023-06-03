42156bc8d73ad146460a6f48785ae732aea27984
Bekmurod Boqiyev <bekmurodboqiyev21@gmail.com>
2023-06-03 07:58:49 +0500

    District,Region Mutation completed

--- src/FleetFlow.GraphQL/Queries/Query.Payment.cs
+++ src/FleetFlow.GraphQL/Queries/Query.Payment.cs
@@ -1,6 +1,18 @@
-﻿namespace FleetFlow.GraphQL.Queries
+﻿using FleetFlow.Domain.Congirations;
+using FleetFlow.Service.DTOs.Payments;
+using FleetFlow.Service.Interfaces.Orders;
+
+namespace FleetFlow.GraphQL.Queries
 {
-    public class Query
+    public partial class Query
     {
+        public async ValueTask<PaymentResultDto> GetPaymentByIdAsync([Service] IPaymentService service, long id)
+        {
+            return await service.RetrieveByIdAsync(id);
+        }
+        public async ValueTask<IEnumerable<PaymentResultDto>> GetPaymentAllAsync([Service] IPaymentService service,PaginationParams @params)
+        {
+            return await service.RetrieveAllAsync(@params);
+        }
     }
 }
--- src/FleetFlow.GraphQL/Queries/Query.Product.cs
+++ src/FleetFlow.GraphQL/Queries/Query.Product.cs
@@ -1,6 +1,18 @@
-﻿namespace FleetFlow.GraphQL.Queries
+﻿using FleetFlow.Domain.Congirations;
+using FleetFlow.Service.DTOs.Product;
+using FleetFlow.Service.Interfaces.Products;
+
+namespace FleetFlow.GraphQL.Queries
 {
-    public class Query
+    public partial class Query
     {
+        public async ValueTask<ProductForResultDto> GetProductByIdAsync([Service] IProductService service,long id)
+        {
+            return await service.RetrieveByIdAsync(id);
+        }
+        public async ValueTask<IEnumerable<ProductForResultDto>> GetProductAllAsync([Service] IProductService service,PaginationParams @params)
+        {
+            return await service.RetrieveAllAsync(@params);
+        }
     }
 }
